e789e6c9fb480a2e32ec24bbea17cb589530d2df
Kevin Egan <kevinegan31@gmail.com>
2022-12-19 17:41:31 -0600

    Create EXERCISE-LL-Constructor.py

---
+++ Linked_Lists/EXERCISE-LL-Constructor.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    #                               #
+    #                               #
+    #                               #
+    #                               #
+    #################################
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    ## WRITE LL CONSTRUCTOR HERE ##
+    #                             #
+    #                             #
+    #                             #
+    #                             #
+    ###############################
+
+
+
+ 
+my_linked_list = LinkedList(4)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 4
+    Tail: 4
+    Length: 1
+    
+"""
+
+                                                                                                                    
