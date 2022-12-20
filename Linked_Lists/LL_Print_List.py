554e1108938469e83dfe501f4d4ea72306acf356
Kevin Egan <kevinegan31@gmail.com>
2022-12-20 16:08:05 -0600

    Editing files

---
+++ Linked_Lists/LL_Print_List.py
@@ -0,0 +1,6 @@
+# LL Print List
+def print_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.value)
+        temp = temp.next
--- Linked_Lists/Linked_List_Constructor.py
+++ Linked_Lists/Linked_List_Constructor.py
@@ -16,11 +16,25 @@ class LinkedList:
         self.head = new_node # points to new node
         self.tail = new_node # points to new node
         self.length = 1
-    
-    # def append(self, value):
-    #     # Create new Node
-    #     # Add new node to the end
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        # Create new Node
+        new_node = Node(value)
+        # Add new node to the end
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
     # def prepend(self, value):
     #     # Create new Node
     #     # Add new node to the beginning
@@ -28,6 +42,10 @@ class LinkedList:
     # def insert(self, value):
     #     # Create new Node
     #     # Insert node wherever requested
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.print_list()
 
 my_linked_list = LinkedList(4)
 print(my_linked_list.head.value)
