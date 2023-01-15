78fc90d70f030a45e12d00e788b3f911afd54e99
Kevin Egan <kevinegan31@gmail.com>
2023-01-15 13:46:22 +0000

    Adding Recursion

---
+++ Recursion/CallStack.py
@@ -0,0 +1,18 @@
+"""Runs funcOne"""
+
+
+def funcThree():
+    print("Three")
+
+
+def funcTwo():
+    funcThree()
+    print("Two")
+
+
+def funcOne():
+    funcTwo()
+    print("One")
+
+
+funcOne()
